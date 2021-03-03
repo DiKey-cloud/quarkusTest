843958fb1fa40ee075aa3630904faba55ffb2369
Irina <iraginger93@yandex.ru>
2021-03-03 13:08:54 +0300

    Initial commit

---
+++ src/main/java/HelloResource.java
@@ -0,0 +1,22 @@
+import io.quarkus.security.Authenticated;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.security.Principal;
+
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Path("/")
+//    @Authenticated
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@Context SecurityContext context) {
+        Principal userPrincipal = context.getUserPrincipal();
+        return "Hello, " + 123 + "!";
+    }
+}
